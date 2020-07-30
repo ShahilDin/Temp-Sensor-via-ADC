318af5d56152528016409518651e634a49cb1b0e
Shahil Din <dinshahil@gmail.com>
2020-07-31 00:07:02 +1200

    Add files via upload

--- main.c
+++ main.c
@@ -1,25 +1,20 @@
 /*
- * Temp Sen with Heater ADC 3.0.c
+ * ACD 4.0 LDR.c
  *
- * Created: 9/02/2020 9:57:18 PM
+ * Created: 16/02/2020 12:04:22 PM
  * Author : bulle
  */ 
-
-/*
- * Learning ADC 2.0.c
- *
- * Created: 9/02/2020 3:06:20 PM
- * Author : bulle
- */ 
-
-
-
 
 #define F_CPU 8000000UL // Clock SpeedThis is a macro that will be used by the compiler to calculate delay loops, and states it to be 8MHz, without this line the program defaults to some other value (1000000) and all the timing would be wrong.
 #include <util/delay.h> // included to use delay function
 #include <avr/io.h> // include the chip
 
 void setup(void);
+
+#define startcon ADCSRA |= (1<<ADSC) // starting conversion 
+#define display_ PORTC // 8 seg display
+#define Heater_ON PORTD |= (1<<PD7) // turning on bit 7
+#define Heater_OFF PORTD &= ~(1<<PD7) // Turning off bit 7 
  
 int main(void)
 {
@@ -32,20 +27,28 @@ int main(void)
     while (1) 
     {
 		 
+		startcon; // start conversion
+	     while (startcon)
+		 {
+			// When there is zero light source the 7 seg show 00 but as we show light to the LDR it increases
+			display_ =ADCH; // display it on 7 seg 
+					 
+			if (ADCH <= 0x07) // if its less or = to 7 then
+			{
+				Heater_ON; // turn on heater
+			
+			}
+			else 
+			{
+				Heater_OFF; // heater off
+			
+			}
 		
-	      
+		 }
 		  
-		  if(PINA&(1<<4)) // testing for pina 4 if true then completed 
-		  {
-			  PORTD=0b10000000; // turn on heater 
-		  }
-		  else
-		  {
-			  PORTD=0x00; // turning off heater
-		  }
+		
 		  
 		  
-		  PORTC=ADCH; // PORCT will display the conversion on 8seg 
 		
 	}
     
@@ -55,16 +58,15 @@ void setup(void)
 {
 	DDRC=0xFF; // Setting PORTC as outputs(LED)
 	PORTC=0x00;// Setting PORTA as inputs (Switches)
-	DDRD=0xFF; // Heater is on port d so output
-	DDRA=0x00;// for button input
-	
+	DDRD=0xFF; // as ouput as heater led is on PORTD 
 
-		
-	//ADMUX=0b01100010;  another way to write this is below
-	//ADCSRA=0b10000111;
-	ADMUX = (1<<REFS0) | (1<<ADLAR) | (1<<MUX1) | (1<<MUX0);// AAvcc , left adjusted , ADC3
+
+	ADMUX |= (1<< REFS0) | (1<<ADLAR)  ; // AVV with external cap at  as REF, Left Adjusted , ADC0
 	
-	ADCSRA = (1<<ADEN) |(1<<ADSC) | (1<<ADATE) | (1<<ADPS2) |(1<<ADPS1) | (1<<ADPS0); // Enable ADC, Setting prescaler 128, Started conversion, AUTO enable triggers every pos edge
+	ADCSRA |= (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1)| (1<<ADPS0) ; // Enable ADC .. 128 Prescaler 
+	
+	
+	
 	
 	
 }
